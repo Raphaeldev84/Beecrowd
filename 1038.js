e46e333918bf5802af78542532594266676a78fd
Raphael <125095194+Raphaeldev84@users.noreply.github.com>
2023-11-03 17:27:02 -0300

    Exercícios Beecrowd

---
+++ 1038.js
@@ -0,0 +1,28 @@
+//importo o modulo fs (filesystem) para trabalhar com arquivos e diretorios
+const fileSystem = require('fs')
+
+//importo o modulo path para trabalhar com o endereço de arquivos e diretorios
+const endereco = require('path');
+
+//descubro o endereço de onde meu script js esta salvo
+//ex: c:\documentos\javascript
+const enderecoDesteScriptJS = endereco.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin que foi criado
+const conteudoArquivo = fileSystem.readFileSync(enderecoDesteScriptJS + '\\dev\\stdin', 'utf8');
+
+//define qual é a quebra de linha utilizada no seu sistema operacional
+//se você estiver utilizando linux coloque \n se for windows coloque \r\n
+const quebraLinhaWindows = '\r\n'
+
+let lines = conteudoArquivo.split(quebraLinhaWindows);
+
+///////////////////////COLAR NO BEECROWD O CODIGO DESTE PONTO EM DIANTE///////////////////
+//para ler linhas com mais de um valor na mesma linha use o comando a seguir
+let [codigo, quantidade] = lines.shift().split(" ").map((index) => parseInt(index)); 
+
+let precos = [4.00, 4.50, 5.00, 2.00, 1.50];
+
+let total = precos[codigo - 1] * quantidade;
+
+console.log("Total: R$ " + total.toFixed(2));
