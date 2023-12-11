8097173122e5265d60c18ad47b72993dfd97e2fe
Raphael <125095194+Raphaeldev84@users.noreply.github.com>
2023-12-11 15:20:40 -0300

    Resolução dos exercícios 1114, 1116, 1117

---
+++ 1116.js
@@ -0,0 +1,33 @@
+//importo o modulo fs (filesystem) para trabalhar com arquivos e diretorios
+const fileSystem = require('fs')
+
+//importo o modulo path para trabalhar com o endereço de arquivos e diretorios
+const endereco = require('path');
+
+//descubro o endereço de onde meu script js esta salvo
+//ex: c:\documentos\javascript
+const enderecoDesteScriptJS = endereco.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin que foi criado
+const conteudoArquivo = fileSystem.readFileSync(enderecoDesteScriptJS + '\\dev\\stdin', 'utf8');
+
+//define qual é a quebra de linha utilizada no seu sistema operacional
+//se você estiver utilizando linux coloque \n se for windows coloque \r\n
+const quebraLinhaWindows = '\r\n'
+
+let lines = conteudoArquivo.split(quebraLinhaWindows);
+
+///////////////////////COLAR NO BEECROWD O CODIGO DESTE PONTO EM DIANTE///////////////////
+//para ler linhas com mais de um valor na mesma linha use o comando a seguir
+
+let n = parseInt(lines.shift());   
+
+for(let i = 0; i < n; i++ ){
+    let [x, y] = lines[i].split(" ").map(Number);
+    if(y === 0) {
+        console.log("divisao impossivel");
+    }else{
+        let soma = x / y
+        console.log(soma.toFixed(1))
+    }
+}
